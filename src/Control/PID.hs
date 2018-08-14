2a380ae484ed252bdd4cbf3e3e4d3436ec3fad41
Jonathan Lamothe <jlamothe1980@gmail.com>
2018-08-14 14:50:46 -0400

    lensified

--- src/Control/PID.hs
+++ src/Control/PID.hs
@@ -1,59 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Control.PID
     ( PIDSettings (..)
     , PIDStatus
     , newPIDSettings
     , newPIDStatus
-    , HasPIDSettings
     ) where
+
+import Control.Lens
 
 data PIDSettings a =
   PIDSettings
-  { pidSetpoint      :: a
-  , pidPFactor       :: a
-  , pidIFactor       :: a
-  , pidDFactor       :: a
-  , pidBias          :: a
-  , pidReversed      :: Bool
-  , pidMaxOutput     :: a
-  , pidIntegralLimit :: a
+  { _pidSetpoint      :: a
+  , _pidPFactor       :: a
+  , _pidIFactor       :: a
+  , _pidDFactor       :: a
+  , _pidBias          :: a
+  , _pidReversed      :: Bool
+  , _pidMaxOutput     :: a
+  , _pidIntegralLimit :: a
   } deriving (Eq, Show)
 
 data PIDStatus a =
   PIDStatus
-  { pidSettings     :: PIDSettings a
-  , pidLastError    :: a
-  , pidLastIntegral :: a
+  { _pidSettings     :: PIDSettings a
+  , _pidLastError    :: a
+  , _pidLastIntegral :: a
   } deriving (Eq, Show)
+
+makeLenses ''PIDSettings
+makeLenses ''PIDStatus
 
 newPIDSettings :: Fractional a => PIDSettings a
 newPIDSettings = PIDSettings 0 1 1 1 0 False 100 100
 
 newPIDStatus :: Fractional a => PIDStatus a
 newPIDStatus = PIDStatus newPIDSettings 0 0
-
-class HasPIDSettings t where
-  getPIDSettings :: t a -> PIDSettings a
-  setPIDSettings :: PIDSettings a -> t a -> t a
-
-class HasPIDStatus t where
-  getPIDStatus :: t a -> PIDStatus a
-  setPIDStatus :: PIDStatus a -> t a -> t a
-
-instance HasPIDSettings PIDSettings where
-  getPIDSettings     = id
-  setPIDSettings x _ = x
-
-instance HasPIDSettings PIDStatus where
-  getPIDSettings = pidSettings
-  setPIDSettings x s = s { pidSettings = x }
-
-instance HasPIDStatus PIDStatus where
-  getPIDStatus = id
-  setPIDStatus x _ = x
-
-changePIDSettings
-  :: HasPIDSettings t
-  => (PIDSettings a -> PIDSettings a)
-  -> t a
-  -> t a
-changePIDSettings f x = setPIDSettings (f $ getPIDSettings x) x
