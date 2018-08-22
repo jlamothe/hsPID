d1a679b815692ca20b58608b0a51a25bd544fadc
Jonathan Lamothe <jlamothe1980@gmail.com>
2018-08-22 18:18:13 -0400

    Settings tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,12 @@ import Control.Monad
 import System.Exit
 import Test.HUnit
 
+import qualified Settings
+
 main :: IO ()
 main = do
   counts <- runTestTT tests
   when (failures counts > 0 || errors counts > 0) exitFailure
 
 tests :: Test
-tests = TestList []
+tests = TestList [Settings.tests]
