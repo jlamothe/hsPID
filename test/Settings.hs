fec8f3a6b700709ac3063d2ef2be7450849bda95
Jonathan Lamothe <jlamothe1980@gmail.com>
2018-08-22 18:47:35 -0400

    renamed reversePID to reversed

--- test/Settings.hs
+++ test/Settings.hs
@@ -3,7 +3,7 @@ module Settings where
 import Control.Lens
 import Test.HUnit
 
-import Control.PID
+import qualified Control.PID as PID
 
 tests :: Test
 tests = TestLabel "Settings" $ TestList
@@ -12,13 +12,13 @@ tests = TestLabel "Settings" $ TestList
 
 newSettingsTests :: Test
 newSettingsTests = TestLabel "newSettings" $ TestList $
-  [ TestLabel "setpoint" $   new^.setpoint   ~?= 0
-  , TestLabel "pFactor" $    new^.pFactor    ~?= 1
-  , TestLabel "pidIFactor" $ new^.iFactor    ~?= 1
-  , TestLabel "dFactor" $    new^.dFactor    ~?= 1
-  , TestLabel "bias" $       new^.bias       ~?= 0
-  , TestLabel "reversePID" $ new^.reversePID ~?= False
-  , TestLabel "maxOutput" $  new^.maxOutput  ~?= 100
+  [ TestLabel "setpoint" $   new^.PID.setpoint  ~?= 0
+  , TestLabel "pFactor" $    new^.PID.pFactor   ~?= 1
+  , TestLabel "pidIFactor" $ new^.PID.iFactor   ~?= 1
+  , TestLabel "dFactor" $    new^.PID.dFactor   ~?= 1
+  , TestLabel "bias" $       new^.PID.bias      ~?= 0
+  , TestLabel "reversePID" $ new^.PID.reversed  ~?= False
+  , TestLabel "maxOutput" $  new^.PID.maxOutput ~?= 100
   ]
 
-new = newSettings :: Settings Rational
+new = PID.newSettings :: PID.Settings Rational
